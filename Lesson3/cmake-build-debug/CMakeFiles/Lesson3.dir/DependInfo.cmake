dc4f8f90599871fea9922aab1140de0177b0e8bb
Кирилл Королёв <kingstertime@gmail.com>
2020-09-20 15:01:40 +0300

    add Lesson3

---
+++ Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/task10.cpp" "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/task10.cpp.o"
+  "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/task11.cpp" "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/task11.cpp.o"
+  "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/task12.cpp" "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/task12.cpp.o"
+  "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/task9.cpp" "/Users/kirill/CLionProjects/parallel-programming-2020/Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/task9.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lesson3/cmake-build-debug/CMakeFiles/Lesson3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson3.dir/task10.cpp.o"
+  "CMakeFiles/Lesson3.dir/task11.cpp.o"
+  "CMakeFiles/Lesson3.dir/task12.cpp.o"
+  "CMakeFiles/Lesson3.dir/task9.cpp.o"
+  "Lesson3"
+  "Lesson3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
